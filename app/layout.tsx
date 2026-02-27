584b5bcc2276f71fcef1784f368c9814cee2a1ef
AARublev <106831151+AARublev@users.noreply.github.com>
2026-02-27 11:38:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'  // ← добавь импорт
 
 import './globals.css'
 
@@ -18,7 +19,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${inter.variable} font-sans antialiased`}>{children}</body>
+      <body className={`${inter.variable} font-sans antialiased`}>
+        {/* VK Pixel */}
+        <Script id="vk-pixel" strategy="afterInteractive">{`
+          var _tmr = window._tmr || (window._tmr = []);
+          _tmr.push({id: "3745662", type: "pageView", start: (new Date()).getTime()});
+          (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+          })(document, window, "tmr-code");
+        `}</Script>
+        <noscript>
+          <div>
+            <img src="https://top-fwz1.mail.ru/counter?id=3745662;js=na" style={{position:'absolute',left:'-9999px'}} alt="Top.Mail.Ru" />
+          </div>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   )
 }
