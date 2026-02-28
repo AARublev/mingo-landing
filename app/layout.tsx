ed251bc9ad2d28098d22643d8fc336fda2277ff8
AARublev <106831151+AARublev@users.noreply.github.com>
2026-02-28 11:31:36 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        {/* VK Pixel */}
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
           var _tmr = window._tmr || (window._tmr = []);
           _tmr.push({id: "3745662", type: "pageView", start: (new Date()).getTime()});
@@ -32,6 +33,22 @@ export default function RootLayout({
         <noscript>
           <div>
             <img src="https://top-fwz1.mail.ru/counter?id=3745662;js=na" style={{position:'absolute',left:'-9999px'}} alt="Top.Mail.Ru" />
+          </div>
+        </noscript>
+
+        {/* Yandex Metrika */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107047349', 'ym');
+          ym(107047349, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}} />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/107047349" style={{position:'absolute', left:'-9999px'}} alt="" />
           </div>
         </noscript>
       </head>
